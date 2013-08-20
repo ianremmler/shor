88ed18fa39320a8063147d8059d484c26c44164c
Ian Remmler <ian@remmler.org>
2013-08-20 13:09:15 -0500

    Added format flags to shor command.

--- cmd/shor/shor.go
+++ cmd/shor/shor.go
@@ -3,15 +3,32 @@ package main
 import (
 	"github.com/ianremmler/shor"
 
+	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
+	isSingle, indent := false, "\t"
+	flag.BoolVar(&isSingle, "s", isSingle, "Produce single-line output")
+	flag.StringVar(&indent, "i", indent, "String used to indent one level")
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "usage: shor [-s] [-i indentstr] < input.shor")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
 	tree, err := shor.Parse(os.Stdin)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	fmt.Println(tree)
+	depth := 0
+	if isSingle {
+		depth = -1
+	}
+	fmt.Print(tree.Format(depth, indent))
+	if isSingle {
+		fmt.Println()
+	}
 }
